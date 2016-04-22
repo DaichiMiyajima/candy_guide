23e100b10d0b68cfd5c4808b40ca6ec8128bd482
MiyajimaDaichi <let.it.be7daichi@gmail.com>
2016-04-22 23:40:44 +0900

    remove vain code

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,8 @@
 var express = require('express');
-var request = require('request');
-var Firebase = require("firebase");
-var ref = new Firebase("https://candyguide.firebaseio.com");
 var router = express.Router();
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-	var authData = ref.getAuth();
-	console.log("authData(index):"+authData);
-	//console.log("authData(req):"+req);
-
-	//ヘッダーを定義
-	var headers = {
-		'Content-Type':'application/json'
-	}
-
-	//オプションを定義
-	var options = {
-		url: 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCO32axBK69PMVZMgs_oPjKMltoQa4pKI4',
-		method: 'POST',
-		headers: headers,
-		json: true
-	}
-
-	//リクエスト送信
-	request(options, function (error, response, body) {
-		//コールバックで色々な処理
-		//console.log('body::::::::::::::'+JSON.stringify(body));
-	});
-
 	/* Google Map Screen*/	
 	res.render('index', { title: 'bad' });
 });
--- routes/login.js
+++ routes/login.js
@@ -1,31 +1,10 @@
 var express = require('express');
-var request = require('request');
-var Firebase = require("firebase");
 var router = express.Router();
 
 
 /* GET home page. */
 router.get('/login', function(req, res, next) {
-	//ヘッダーを定義
-	var headers = {
-		'Content-Type':'application/json'
-	}
-
-	//オプションを定義
-	var options = {
-		url: 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCO32axBK69PMVZMgs_oPjKMltoQa4pKI4',
-		method: 'POST',
-		headers: headers,
-		json: true
-	}
-
-	//リクエスト送信
-	request(options, function (error, response, body) {
-		//コールバックで色々な処理
-		console.log('body::::::::::::::'+JSON.stringify(body));
-	});
-
-/* Google Map Screen*/	
-res.render('login', { title: 'login' });
+	/* Google Map Screen*/	
+	res.render('login', { title: 'login' });
 });
 module.exports = router;
